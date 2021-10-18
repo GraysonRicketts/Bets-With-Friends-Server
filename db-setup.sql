12ad920929bbf84bab86f4786c1b226d0b3d7d49
GraysonRicketts <grayson.ricketts@gmail.com>
2021-10-17 21:01:42 -0500

    db setup stuff

---
+++ db-setup.sql
@@ -0,0 +1,7 @@
+CREATE USER server_user;
+ALTER USER server_user WITH PASSWORD ;
+
+CREATE DATABASE bets_with_friends;
+GRANT CONNECT ON DATABASE bets_with_friends TO server_user;
+GRANT USAGE ON SCHEMA public TO server_user;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO server_user;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join, resolve } from 'path';
 import { CustomDbLogger } from 'src/logger/CustomDbLogger';
 import { CustomLogger } from 'src/logger/CustomLogger';
 import { LoggerModule } from 'src/logger/Logger.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+console.log(join(resolve(), 'src/migration'))
+console.log(resolve())
 @Module({
   imports: [
     LoggerModule,
@@ -13,7 +16,7 @@ import { AppService } from './app.service';
     TypeOrmModule.forRoot({
       retryAttempts: 0,
       useUTC: true,
-      logger: new CustomDbLogger(new CustomLogger('TypeOrm'))
+      logger: new CustomDbLogger(new CustomLogger('TypeOrm')),
     }),
   ],
   controllers: [AppController],
