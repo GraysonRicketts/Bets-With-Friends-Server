ca194847e3110f1155a5198f4261ff25ae5feac7
GraysonRicketts <grayson.ricketts@gmail.com>
2021-10-28 17:08:37 -0500

    primSetup base prisma stuff

--- db-setup.sql
+++ db-setup.sql
@@ -5,3 +5,6 @@ CREATE DATABASE bets_with_friends;
 GRANT CONNECT ON DATABASE bets_with_friends TO server_user;
 GRANT USAGE ON SCHEMA public TO server_user;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO server_user;
+ALTER USER server_user CREATEDB;
+
+SET TIMEZONE='GMT';
