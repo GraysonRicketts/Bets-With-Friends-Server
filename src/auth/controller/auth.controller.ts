0046623c74a684fe83d8bfa6882cd40414a83cc6
GraysonRicketts <grayson.ricketts@gmail.com>
2022-01-06 09:07:56 -0600

    save google auth wip

--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Request, Post, UseGuards, Body } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Body, Get } from '@nestjs/common';
 import { CreateLocalUserDto } from '../dto/create-user.dto';
 import { LocalAuthGuard } from '../guard/local-auth.guard';
+import { GoogleAuthGuard } from '../guard/google-auth.guard';
 import { AuthService } from '../service/auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @UseGuards(GoogleAuthGuard)
+  @Get('google')
+  async loginGoogle() {}
+
+  @UseGuards(GoogleAuthGuard)
+  @Get('google/callback')
+  async loginGoogleRedirect(@Request() req) {
+    // return await this.authService.login(req.user);
+    return true;
+  }
+  
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
--- src/env/env.constants.ts
+++ src/env/env.constants.ts
@@ -4,3 +4,6 @@ export const NODE_ENV = process.env.NODE_ENV;
 export const CIPHER_SECRET = process.env.CIPHER_SECRET;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const JWT_EXPIRATION = process.env.JWT_EXPIRATION;
+export const GOOGLE_OAUTH_CLIENT_ID = process.env.GOOGLE_OAUTH_CLIENT_ID;
+export const GOOGLE_OAUTH_CLIENT_SECRET = process.env.GOOGLE_OAUTH_CLIENT_SECRET;
+export const GOOGLE_OAUTH_CALLBACK = process.env.GOOGLE_OAUTH_CALLBACK;
